d6b051ded920a579cb331c3a05d4eee8e8d02d4f
Carmen Rey <creyvilas@gmail.com>
2023-08-12 08:44:32 +0200

    Subiendo archivos

---
+++ config.py
@@ -0,0 +1,5 @@
+from PIL import Image
+
+PAGE_CONFIG = {"page_title"            : "Grupo Hotusa Scientist Test",
+               "page_icon"             : Image.open("data/minilogo.jpg"),
+               "initial_sidebar_state" : "collapsed"}
